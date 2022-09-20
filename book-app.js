e111d74a4f36aea5af8f88e590d39f2cfe84c78b
levib00 <levib8@hotmail.com>
2022-09-20 00:05:58 -0400

    create console function

---
+++ book-app.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pageCount, status) {
+    this.title = title
+    this.author = author
+    this.pageCount = pageCount
+    this.status = status
+    this.info = function() {
+        return `${title} by ${author}, ${pageCount} pages, ${status}`
+        }
+}
+
+function addBookToLibrary() {
+    
+}
