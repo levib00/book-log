2957d815cdef20e227d24ae6be88791bd9062caf
levib00 <levib8@hotmail.com>
2022-09-26 22:06:27 -0400

    Improve readability

--- book-app.js
+++ book-app.js
@@ -49,105 +49,108 @@ function createListItem() {
     const listContainer = document.getElementById("booklist-container");
 
     const listItemContainer = document.createElement("div");
-    listItemContainer.setAttribute("class","booklist-item");
+    listItemContainer.setAttribute("class","booklist-item grid-container");
     listItemContainer.setAttribute("id", `${i}listItemContainer`)
     listContainer.append(listItemContainer);
     
     const listItemTitle = document.createElement("div");
-    listItemTitle.setAttribute("class","book-info");
+    listItemTitle.setAttribute("class","book-info flex-container");
     listItemContainer.append(listItemTitle);
     listItemTitle.innerHTML = myLibrary[i].title;
 
     const listItemAuthor = document.createElement("div");
     listItemAuthor.innerHTML = myLibrary[i].author;
-    listItemAuthor.setAttribute("class","book-info");
+    listItemAuthor.setAttribute("class","book-info flex-container");
     listItemContainer.append(listItemAuthor);
 
     const listItemPages = document.createElement("div");
     listItemPages.innerHTML = myLibrary[i].pageCount;
-    listItemPages.setAttribute("class","book-info");
+    listItemPages.setAttribute("class","book-info flex-container");
     listItemContainer.append(listItemPages);
 
     listItemStatus  = document.createElement("div");
-    listItemStatus.setAttribute("class","book-info");
+    listItemStatus.setAttribute("class","book-info flex-container");
     listItemStatus.setAttribute("id", "checkbox-align")
     listItemContainer.append(listItemStatus);
-    
-    const readContainer = document.createElement("li");
-    readContainer.setAttribute("class","checkbox-container");
-    listItemStatus.append(readContainer);
-
-        const readLabel = document.createElement("Label");
-        readLabel.setAttribute("name", "read-box")
-        readLabel.innerHTML = "Read"
-        readContainer.append(readLabel)
-
-        readCheckbox = document.createElement("input");
-        readCheckbox.setAttribute("type", "radio")
-        readCheckbox.setAttribute("id", "read-box")
-        readCheckbox.setAttribute("name", `checkbox${i}`)
-        readContainer.append(readCheckbox)
-
-    const unreadContainer = document.createElement("li");
-    unreadContainer.setAttribute("class","checkbox-container");
-    listItemStatus.append(unreadContainer);
-
-        const unreadLabel = document.createElement("Label");
-        unreadLabel.setAttribute("name", "unread-box")
-        unreadLabel.innerHTML = "Unread"
-        unreadContainer.append(unreadLabel)
-
-        unreadCheckbox = document.createElement("input");
-        unreadCheckbox.setAttribute("type", "radio")
-        unreadCheckbox.setAttribute("id", "unread-box")
-        unreadCheckbox.setAttribute("name", `checkbox${i}`)
-        unreadContainer.append(unreadCheckbox)
-
-    const dnfContainer = document.createElement("li");
-    dnfContainer.setAttribute("class","checkbox-container");
-    listItemStatus.append(dnfContainer);
-
-        const dnfLabel = document.createElement("Label");
-        dnfLabel.setAttribute("name", "dnf-box")
-        dnfLabel.innerHTML = "DNF"
-        dnfContainer.append(dnfLabel)
-
-        dnfCheckbox = document.createElement("input");
-        dnfCheckbox.setAttribute("type", "radio")
-        dnfCheckbox.setAttribute("id", "dnf-box")
-        dnfCheckbox.setAttribute("name", `checkbox${i}`)
-        dnfContainer.append(dnfCheckbox)
-
-    const readingContainer = document.createElement("li");
-    readingContainer.setAttribute("class","checkbox-container");
-    listItemStatus.append(readingContainer);
-
-        const readingLabel = document.createElement("Label");
-        readingLabel.setAttribute("name", "reading-box")
-        readingLabel.innerHTML = "Reading"
-        readingContainer.append(readingLabel)
-
-        readingCheckbox = document.createElement("input");
-        readingCheckbox.setAttribute("type", "radio")
-        readingCheckbox.setAttribute("id", "reading-box")
-        readingCheckbox.setAttribute("name", `checkbox${i}`)
-        readingContainer.append(readingCheckbox)
 
     const delButtonContainer = document.createElement("div")
     delButtonContainer.setAttribute("id","align-delete");
-    delButtonContainer.setAttribute("class", "book-info")
+    delButtonContainer.setAttribute("class", "book-info flex-container")
     listItemContainer.append(delButtonContainer)
 
     const delButton = document.createElement("button");
     delButton.setAttribute("type","button");
     delButton.setAttribute("class","del-button button");
-    delButton.innerHTML = "Del";
+    delButton.innerHTML = "Delete";
     delButtonContainer.append(delButton);
 
     const deleteButton = document.querySelectorAll(".del-button")
     deleteButton.forEach((deleteButtons) => {
     deleteButtons.addEventListener('click', deleteListItem);
-  });
+});
+    createCheckboxes();
+}
+
+function createCheckboxes() {
+    const readContainer = document.createElement("li");
+    readContainer.setAttribute("class","checkbox-container flex-container");
+    listItemStatus.append(readContainer);
+
+    const readLabel = document.createElement("Label");
+    readLabel.setAttribute("name", "read-box")
+    readLabel.innerHTML = "Read"
+    readContainer.append(readLabel)
+
+    readCheckbox = document.createElement("input");
+    readCheckbox.setAttribute("type", "radio")
+    readCheckbox.setAttribute("id", "read-box")
+    readCheckbox.setAttribute("name", `checkbox${i}`)
+    readContainer.append(readCheckbox)
+
+    const unreadContainer = document.createElement("li");
+    unreadContainer.setAttribute("class","checkbox-container flex-container");
+    listItemStatus.append(unreadContainer);
+
+    const unreadLabel = document.createElement("Label");
+    unreadLabel.setAttribute("name", "unread-box")
+    unreadLabel.innerHTML = "Unread"
+    unreadContainer.append(unreadLabel)
+
+    unreadCheckbox = document.createElement("input");
+    unreadCheckbox.setAttribute("type", "radio")
+    unreadCheckbox.setAttribute("id", "unread-box")
+    unreadCheckbox.setAttribute("name", `checkbox${i}`)
+    unreadContainer.append(unreadCheckbox)
+
+    const dnfContainer = document.createElement("li");
+    dnfContainer.setAttribute("class","checkbox-container flex-container");
+    listItemStatus.append(dnfContainer);
+
+    const dnfLabel = document.createElement("Label");
+    dnfLabel.setAttribute("name", "dnf-box")
+    dnfLabel.innerHTML = "DNF"
+    dnfContainer.append(dnfLabel)
+
+    dnfCheckbox = document.createElement("input");
+    dnfCheckbox.setAttribute("type", "radio")
+    dnfCheckbox.setAttribute("id", "dnf-box")
+    dnfCheckbox.setAttribute("name", `checkbox${i}`)
+    dnfContainer.append(dnfCheckbox)
+
+    const readingContainer = document.createElement("li");
+    readingContainer.setAttribute("class","checkbox-container flex-container");
+    listItemStatus.append(readingContainer);
+
+    const readingLabel = document.createElement("Label");
+    readingLabel.setAttribute("name", "reading-box")
+    readingLabel.innerHTML = "Reading"
+    readingContainer.append(readingLabel)
+
+    readingCheckbox = document.createElement("input");
+    readingCheckbox.setAttribute("type", "radio")
+    readingCheckbox.setAttribute("id", "reading-box")
+    readingCheckbox.setAttribute("name", `checkbox${i}`)
+    readingContainer.append(readingCheckbox)
 }
 
 const newBookButton = document.getElementById("new-book-button");
@@ -191,7 +194,6 @@ function addNewBook() {
         }
     }
 
-    
     addBookToLibrary(title, author, pages, status);
     selectRadioButton();
     closeForm();
