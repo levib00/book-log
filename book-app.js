99e8b553ce7be7ac8584fd6defe2615b171870be
levib00 <levib8@hotmail.com>
2022-09-26 21:13:58 -0400

    Change status text to radio buttons, and added reading option

--- book-app.js
+++ book-app.js
@@ -26,7 +26,6 @@ function addBookToLibrary(title, author, pages, status) {
     const book = new Book(title, author, pages, status);
     myLibrary.push(book);
     createListItem();
-    console.log(myLibrary);
     i += 1;
 }
 
@@ -40,6 +39,11 @@ function displayList() {
 function deleteListItem() {
     this.parentNode.parentNode.remove();    
 }
+
+let readCheckbox;
+let unreadCheckbox;
+let dnfCheckbox;
+let readingCheckbox;
 
 function createListItem() {
     const listContainer = document.getElementById("booklist-container");
@@ -64,10 +68,70 @@ function createListItem() {
     listItemPages.setAttribute("class","book-info");
     listItemContainer.append(listItemPages);
 
-    const listItemStatus = document.createElement("div");
-    listItemStatus.innerHTML = myLibrary[i].status;
+    listItemStatus  = document.createElement("div");
     listItemStatus.setAttribute("class","book-info");
+    listItemStatus.setAttribute("id", "checkbox-align")
     listItemContainer.append(listItemStatus);
+    
+    const readContainer = document.createElement("li");
+    readContainer.setAttribute("class","checkbox-container");
+    listItemStatus.append(readContainer);
+
+        const readLabel = document.createElement("Label");
+        readLabel.setAttribute("name", "read-box")
+        readLabel.innerHTML = "Read"
+        readContainer.append(readLabel)
+
+        readCheckbox = document.createElement("input");
+        readCheckbox.setAttribute("type", "radio")
+        readCheckbox.setAttribute("id", "read-box")
+        readCheckbox.setAttribute("name", `checkbox${i}`)
+        readContainer.append(readCheckbox)
+
+    const unreadContainer = document.createElement("li");
+    unreadContainer.setAttribute("class","checkbox-container");
+    listItemStatus.append(unreadContainer);
+
+        const unreadLabel = document.createElement("Label");
+        unreadLabel.setAttribute("name", "unread-box")
+        unreadLabel.innerHTML = "Unread"
+        unreadContainer.append(unreadLabel)
+
+        unreadCheckbox = document.createElement("input");
+        unreadCheckbox.setAttribute("type", "radio")
+        unreadCheckbox.setAttribute("id", "unread-box")
+        unreadCheckbox.setAttribute("name", `checkbox${i}`)
+        unreadContainer.append(unreadCheckbox)
+
+    const dnfContainer = document.createElement("li");
+    dnfContainer.setAttribute("class","checkbox-container");
+    listItemStatus.append(dnfContainer);
+
+        const dnfLabel = document.createElement("Label");
+        dnfLabel.setAttribute("name", "dnf-box")
+        dnfLabel.innerHTML = "DNF"
+        dnfContainer.append(dnfLabel)
+
+        dnfCheckbox = document.createElement("input");
+        dnfCheckbox.setAttribute("type", "radio")
+        dnfCheckbox.setAttribute("id", "dnf-box")
+        dnfCheckbox.setAttribute("name", `checkbox${i}`)
+        dnfContainer.append(dnfCheckbox)
+
+    const readingContainer = document.createElement("li");
+    readingContainer.setAttribute("class","checkbox-container");
+    listItemStatus.append(readingContainer);
+
+        const readingLabel = document.createElement("Label");
+        readingLabel.setAttribute("name", "reading-box")
+        readingLabel.innerHTML = "Reading"
+        readingContainer.append(readingLabel)
+
+        readingCheckbox = document.createElement("input");
+        readingCheckbox.setAttribute("type", "radio")
+        readingCheckbox.setAttribute("id", "reading-box")
+        readingCheckbox.setAttribute("name", `checkbox${i}`)
+        readingContainer.append(readingCheckbox)
 
     const delButtonContainer = document.createElement("div")
     delButtonContainer.setAttribute("id","align-delete");
@@ -108,15 +172,28 @@ function addNewBook() {
     const pages = document.getElementById("book-pages-in").value;
     const status = document.getElementById("book-status").value;
 
-    console.log(title)
+    console.log(status)
     
     if (title === "" || author === "" || pages === "" || status === "") 
     {
         return
     }
 
-    addBookToLibrary(title, author, pages, status);
+    function selectRadioButton() {
+        if (status === "Read") {
+            readCheckbox.checked = true;
+        } else if (status === "Unread") {
+            unreadCheckbox.checked = true;
+        } else if (status === "DNF") {
+            dnfCheckbox.checked = true;
+        } else if (status === "Reading") {
+            readingCheckbox.checked = true;
+        }
+    }
 
+    
+    addBookToLibrary(title, author, pages, status);
+    selectRadioButton();
     closeForm();
 }
 
